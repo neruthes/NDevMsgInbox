4875bd34c167b9706ab0a53c8e91245a57f32647
Neruthes <i@neruthes.xyz>
2020-06-12 05:55:16 +0000

    now decrypt.sh will produce a local output file

--- decrypt.sh
+++ decrypt.sh
@@ -2,4 +2,5 @@
 
 # Note: The hostname of the current device should be identical to the name of the NTGE private key file name
 
-ntge decrypt -v -p "inbox/`hostname`.ntge" -i `hostname`
+ntge decrypt -v -p "inbox/`hostname`.ntge" -i `hostname` > .decrypted_content
+cat .decrypted_content
